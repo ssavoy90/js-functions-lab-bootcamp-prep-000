22ea0d78ae9fdf7284147e16df6928879eb535ba
Shanelle Savoy <mznellienellz@gmail.com>
2018-05-24 00:16:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holidays, name) {
   return `Happy ${holidays}, ${name}!`
 }
 
